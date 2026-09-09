38b8b00bd6a476003f0881df813207aec70c86b9
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-09-08 23:01:58 -0600

    feat: agrega los componentes resumen-carrito y listado-productos

---
+++ Frontend/src/app/components/listado-productos/listado-productos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-listado-productos',
+  styleUrl: './listado-productos.css',
+  templateUrl: './listado-productos.html',
+})
+export class ListadoProductos {}
---
+++ Frontend/src/app/components/resumen-carrito/resumen-carrito.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-resumen-carrito',
+  styleUrl: './resumen-carrito.css',
+  templateUrl: './resumen-carrito.html',
+})
+export class ResumenCarrito {}
