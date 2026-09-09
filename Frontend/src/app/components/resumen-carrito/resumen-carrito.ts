540f1fa09137a9ed2d01ccb5cc8de5df30f67deb
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-09-08 23:49:22 -0600

    feat: agrega plantillas ngFor, ngIF y binding reactivo

--- Frontend/src/app/components/resumen-carrito/resumen-carrito.ts
+++ Frontend/src/app/components/resumen-carrito/resumen-carrito.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { CarritoService } from '../../services/carrito.service';
 import { ItemCarrito } from '../../models/item-carrito.model';
 
@@ -9,10 +9,23 @@ import { ItemCarrito } from '../../models/item-carrito.model';
 })
 export class ResumenCarritoComponent implements OnInit {
   carrito$!: Observable<ItemCarrito[]>;
+  total$!: Observable<number>;
 
   constructor(private carritoService: CarritoService) {}
 
   ngOnInit(): void {
     this.carrito$ = this.carritoService.carrito$;
+    this.total$ = this.carrito$.pipe(
+      map(items => items.reduce((acc, item) => acc + item.precio * item.cantidad, 0))
+    );
+  }
+
+  onCambiarCantidad(id: number, event: Event): void {
+    const valor = Number((event.target as HTMLInputElement).value);
+    this.carritoService.cambiarCantidad(id, valor);
+  }
+
+  onEliminar(id: number): void {
+    this.carritoService.eliminarProducto(id);
   }
 }
